89b6573460e55de26d03c5f0f7b072c77f940d01
Bulat Murtazin <random.finelife@gmail.com>
2019-05-12 13:15:38 +0300

    Fixed field lengths.

--- api/models.py
+++ api/models.py
@@ -34,12 +34,12 @@ class News(models.Model):
 
 
 class Attachment(models.Model):
-    url = models.URLField()
+    url = models.URLField(max_length=2000)
     owner = models.ForeignKey(User, on_delete=models.PROTECT, blank=True, null=True, related_name='files')
     attached_to = models.ForeignKey(News, on_delete=models.CASCADE, blank=True, null=True, related_name='attachments')
     label = models.CharField(max_length=50, choices=ATTACHMENT_TYPES, default='link')
     uploaded = models.DateTimeField(auto_now_add=True)
-    name = models.TextField(null=True)
+    name = models.CharField(max_length=1000, blank=True, null=True)
 
     def __str__(self):
         return self.owner.email
